85c3db04c94cb6cf417191a13869b2fdaf00f7f4
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2023-01-13 11:14:10 +0100

    dropdown

--- seleccionar imagen.py
+++ seleccionar imagen.py
@@ -5,6 +5,16 @@ import flet as ft
 def main(page: ft.Page):
     page.title("Proyecto Ángela y Nerea")
 
+    dropdown_menu=ft.Dropdown(width=200, options=[
+        ft.dropdown.Option("Madrid") ,
+        ft.dropdown.Option("Barcelona"),
+        ft.dropdown.Option("Atleticpo del Madrid"),
+        ft.dropdown.Option("Atletido de Bilbao"),
+        ft.dropdown.Option("Betis"),
+        ])
+    page.update()
+    page.add(dropdown_menu)
+
 
 
 
