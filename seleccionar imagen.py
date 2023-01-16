f6263f5fb2c6208d88c3c175deef4bc1a0ad28e8
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2023-01-16 11:09:25 +0100

    todas las imágenes

--- seleccionar imagen.py
+++ seleccionar imagen.py
@@ -5,9 +5,13 @@ import flet as ft
 def main(page: ft.Page):
     page.title="Proyecto Ángela y Nerea"
     img = ft.Image(src=f"atleticodebilbao.jpg", width=100, height=100)
+    img2 = ft.Image(src=f"atleticodemadrid.jpg", width=100, height=100)
+    img3 = ft.Image(src=f"betis.jpg", width=100, height=100)
+    img4 = ft.Image(src=f"mardrid.jpg", width=100, height=100)
+    img5 = ft.Image(src=f"barcelona.jpg", width=100, height=100)
 
 
-    page.add(img)
+    page.add(img, img2, img3, img4, img5)
 
 
 
